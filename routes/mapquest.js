b73174756277d7f89fd21f530ffbac0123aa168a
Mateusz Sus <sus.mateusz@gmail.com>
2017-04-27 23:25:44 +0200

    Działa wyszukiwanie jednej trasy.

--- routes/mapquest.js
+++ routes/mapquest.js
@@ -6,15 +6,35 @@ var express = require('express');
 var https = require('https');
 var router = express.Router();
 
-var mapquest_base_url = "http://www.mapquestapi.com/directions/v2/alternateroutes";
+var mapquest_base_url = "https://www.mapquestapi.com/directions/v2/alternateroutes";
 var mapquest_api_key = "4pGVZ2T5yFL5VhWoSgylbTJm2MGN1rgC";
 /* GET home page. */
 router.get('/routes', function(req, res, next) {
+    var coords = req.query.coords;
+    mapquest_url = mapquest_base_url
+        + "?key=" + mapquest_api_key
+        + "&from=" + coords[1] + "," + coords[0]
+        + "&to=" + coords[3] + "," + coords[2]
+    ;
 
-    mapquest_url = mapquest_base_url +
-            "?key=" + mapquest_api_key;
-    var coords = req.query.coords.join();
+    var callback = function(response) {
+        var str = '';
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+            console.log("MAPQUEST RESPONSE: " + str);
+            var routes = JSON.parse(str);
+            console.log(routes.route.shape.shapePoints);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({route: routes.route.shape.shapePoints }));
+        });
+    }
     console.log(coords);
+    console.log("URL: " + mapquest_url);
+    https.request(mapquest_url, callback).end();
  });
 
 
